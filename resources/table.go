6a142ff3a691e2c476ef01cc870fb167582a6733
Josh Arnold <jarnold@gofigg.com>
2023-06-21 16:15:10 -0400

    Remove trailing slash from mod name

--- resources/table.go
+++ resources/table.go
@@ -7,7 +7,7 @@ import (
 	"github.com/apache/arrow/go/v13/arrow"
 	"github.com/cloudquery/plugin-sdk/v3/schema"
 	"github.com/cloudquery/plugin-sdk/v3/transformers"
-	"github.com/ktrysmt/go-bitbucket/"	
+	"github.com/ktrysmt/go-bitbucket"	
 )
 
 func Bitbucket() *schema.Table {
