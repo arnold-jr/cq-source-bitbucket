5c969d42667f691999ace43bf43479cb7f373974
John Casillas <jcasillas@gofigg.com>
2023-06-22 09:36:32 -0600

    Reqs: Updating plugin to pull projects. Haven't tested. [hackathon]

---
+++ resources/repos.go
@@ -0,0 +1,40 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/arnold-jr/cq-source-bitbucket/client"
+	bb "github.com/arnold-jr/cq-source-bitbucket/lib"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/cloudquery/plugin-sdk/v3/schema"
+	"github.com/cloudquery/plugin-sdk/v3/transformers"
+)
+
+func BitbucketRepos() *schema.Table {
+	return &schema.Table{
+		Name:     "bitbucket_repos_table",
+		Resolver: fetchRepos,
+		Transform: transformers.TransformWithStruct(&bb.Repository{}),
+	}
+}
+
+func fetchRepos(ctx context.Context, meta schema.ClientMeta, parent *schema.Resource, res chan<- interface{}) error {
+	c := meta.(*client.Client)
+
+	conf := c.Config
+	repositories, err := bb.GetRepositories(conf.Workspace, conf.Username, conf.Password)
+	
+	spew.Dump(repositories)
+	spew.Dump(err)
+
+	if err != nil {
+		return err
+	}
+
+	for _, value := range repositories {
+		res <- value 
+		fmt.Println(res)	
+	}
+	return nil
+}
