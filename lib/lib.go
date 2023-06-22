0e3b5c61654a2d7bea73a296a96f56f8f7d41aeb
Josh Arnold <jarnold@gofigg.com>
2023-06-22 11:07:12 -0400

    Add users and projects struct types

--- lib/lib.go
+++ lib/lib.go
@@ -183,3 +183,45 @@ func GetRepositories(workspace string, appPass string, appUser string) ([]Reposi
 	}
 	return repos, nil
 }
+
+type GetUsersOutput struct {
+	Size     int    `json:"size"`
+	Page     int    `json:"page"`
+	Pagelen  int    `json:"pagelen"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Values   []struct {
+		Type  string `json:"type"`
+		Links struct {
+			Self struct {
+				Href string `json:"href"`
+				Name string `json:"name"`
+			} `json:"self"`
+		} `json:"links"`
+		User struct {
+			Type string `json:"type"`
+		} `json:"user"`
+		Workspace struct {
+			Type string `json:"type"`
+		} `json:"workspace"`
+	} `json:"values"`
+}
+
+type GetProjectsOutput struct {
+	Values []struct {
+		Name        string `json:"name"`
+		Key         string `json:"key"`
+		ID          int    `json:"id"`
+		Type        string `json:"type"`
+		Public      bool   `json:"public"`
+		Scope       string `json:"scope"`
+		Description string `json:"description"`
+		Namespace   string `json:"namespace"`
+		Avatar      string `json:"avatar"`
+	} `json:"values"`
+	Size          int  `json:"size"`
+	Limit         int  `json:"limit"`
+	Start         int  `json:"start"`
+	IsLastPage    bool `json:"isLastPage"`
+	NextPageStart int  `json:"nextPageStart"`
+}
